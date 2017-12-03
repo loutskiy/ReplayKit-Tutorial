aff39ded5a4f55adc88b14655c73257bde8e5ad4
Mikhail Lutskiy <mikhail@lwts.ru>
2017-12-03 20:58:56 +0300

    Initial Commit

---
+++ ReplayKit Tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReplayKit Tutorial
+//
+//  Created by Михаил Луцкий on 03.12.2017.
+//  Copyright © 2017 Mikhail Lutskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
